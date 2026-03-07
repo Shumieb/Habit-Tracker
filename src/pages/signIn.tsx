ee78f303b8a58b2c832def2b4d9debd440ac402b
Shumieb <Shumiebc@gmail.com>
2026-03-07 21:06:15 +0000

    changed styles on auth pages

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,60 +1,71 @@
-import { useState } from "react"
-import {Link} from "react-router"
+import { useState } from "react";
+import { Link } from "react-router";
 
 function SignIn() {
+  // variables
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-    // variables
-    const[userName, setUserName] = useState("")
-    const[password, setPassword] = useState("")
+  // function to handle sign in
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
 
-    // function to handle sign in
-    const handleSubmit = (e:any) =>{
-        e.preventDefault()
-
-        console.log("handle submit in sign in", userName, password)
-    }
+    console.log("handle submit in sign in", userName, password);
+  };
 
   return (
-    <section className="w-[80%] mx-auto mt-4">
-        <h1 className="text-3xl text-center py-2">Organise your habits!</h1>
-        <div className="home-hr-style mb-6">
-            <hr />
-        </div>
+    <section className="w-[80%] mx-auto mt-4 text-purple-950">
+      <h1 className="text-3xl text-center py-2 font-bold">
+        Organise your habits!
+      </h1>
+      <div className="home-hr-style mb-6">
+        <hr />
+      </div>
 
-        <p className="text-lg text-center mb-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Ab, optio. Temporibus consequuntur repellendus accusamus exercitationem <br /> delectus veniam facere dicta expedita quibusdam reprehenderit libero in, <br />autem repudiandae ipsam quasi, recusandae dolores.</p>
-    
-        <form 
-            className="flex justify-center content-center flex-col w-[50%] mx-auto mb-10"
-            onSubmit={handleSubmit}
+      <p className="text-lg text-center mb-10">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Ab,
+        optio. Temporibus consequuntur repellendus accusamus exercitationem{" "}
+        <br /> delectus veniam facere dicta expedita quibusdam reprehenderit
+        libero in, <br />
+        autem repudiandae ipsam quasi, recusandae dolores.
+      </p>
+
+      <form
+        className="flex justify-center content-center flex-col w-[600px] mx-auto mb-10"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          name="userName"
+          placeholder="Username"
+          className="text-lg p-2 border-2 border-purple-950 rounded-md mb-3 text-purple-950 outline-purple-900"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          className="text-lg p-2 border-2 border-purple-950 rounded-md mb-5 text-purple-950 outline-purple-900"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="Sign In"
+          className="bg-purple-950 text-white py-2 px-6 text-lg rounded-md cursor-pointer"
+        />
+      </form>
+
+      <div className="flex justify-center align-center">
+        <p className="text-lg mx-1">Don't have an account?</p>
+        <Link
+          to="/sign-up"
+          className="text-purple-950 text-lg italic underline mx-1"
         >
-            <input 
-                type="text" 
-                name="userName" 
-                placeholder="Username" 
-                className="text-lg p-2 border-2 border-black rounded-md mb-3"
-                onChange={e => setUserName(e.target.value)}
-            />
-            <input 
-                type="password" 
-                name="password" 
-                placeholder="Password"
-                className="text-lg p-2 border-2 border-black rounded-md mb-5"
-                onChange={e => setPassword(e.target.value)}
-            />
-            <input 
-                type="submit" 
-                value="Sign In"
-                className="bg-gray-950 text-white py-2 px-6 text-lg rounded-md cursor-pointer" 
-            />
-        </form>
-
-        <div className="text-center">
-            <p className="text-lg">Don't have an account?</p>
-            <Link to="/sign-up" className="text-gray-950 text-lg italic underline">Create a Free Account</Link>
-        </div>
-
+          Create a Free Account
+        </Link>
+      </div>
     </section>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,74 +1,80 @@
-import { useState } from "react"
-import { Link } from "react-router"
+import { useState } from "react";
+import { Link } from "react-router";
 
 function SignUp() {
+  // variables
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCPassword] = useState("");
 
-    // variables
-        const[userName, setUserName] = useState("")
-        const[password, setPassword] = useState("")
-        const[cpassword, setCPassword] = useState("")
-    
-        // function to handle signing up a new user
-        const handleSubmit = (e:any) =>{
-            e.preventDefault()
-    
-            console.log("handle submit in sign in", userName, password, cpassword)
-        }
+  // function to handle signing up a new user
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
 
+    console.log("handle submit in sign in", userName, password, cpassword);
+  };
 
   return (
-    <section>
-        <section className="w-[80%] mx-auto mt-4">
-        <h1 className="text-3xl text-center py-2">Organise your habits!</h1>
-        <div className="home-hr-style mb-6">
-            <hr />
-        </div>
+    <section className="w-[80%] mx-auto mt-4 text-purple-950">
+      <h1 className="text-3xl text-center py-2">Organise your habits!</h1>
+      <div className="home-hr-style mb-6">
+        <hr />
+      </div>
 
-        <p className="text-lg text-center mb-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Ab, optio. Temporibus consequuntur repellendus accusamus exercitationem <br /> delectus veniam facere dicta expedita quibusdam reprehenderit libero in, <br />autem repudiandae ipsam quasi, recusandae dolores.</p>
-    
-        <form 
-            className="flex justify-center content-center flex-col w-[50%] mx-auto mb-10"
-            onSubmit={handleSubmit}
+      <p className="text-lg text-center mb-10">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Ab,
+        optio. Temporibus consequuntur repellendus accusamus exercitationem{" "}
+        <br /> delectus veniam facere dicta expedita quibusdam reprehenderit
+        libero in, <br />
+        autem repudiandae ipsam quasi, recusandae dolores.
+      </p>
+
+      <form
+        className="flex justify-center content-center flex-col w-[600px] mx-auto mb-10"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          name="userName"
+          placeholder="Username"
+          className="text-lg p-2 border-2 border-purple-950 rounded-md mb-3 text-purple-950 outline-purple-900"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          className="text-lg p-2 border-2 border-purple-950 rounded-md mb-3 text-purple-950 outline-purple-900"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          name="cpassword"
+          placeholder="Confirm Password"
+          className="text-lg p-2 border-2 border-purple-950 rounded-md mb-5 text-purple-950 outline-purple-900"
+          value={cpassword}
+          onChange={(e) => setCPassword(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="Sign Up"
+          className="bg-purple-950 text-white py-2 px-6 text-lg rounded-md cursor-pointer"
+        />
+      </form>
+
+      <div className="flex justify-center align-center">
+        <p className="text-lg mx-1">Already have an account?</p>
+        <Link
+          to="/sign-in"
+          className="text-purple-950 text-lg italic underline mx-1"
         >
-            <input 
-                type="text" 
-                name="userName" 
-                placeholder="Username" 
-                className="text-lg p-2 border-2 border-black rounded-md mb-3"
-                value={userName}
-                onChange={e => setUserName(e.target.value)}
-            />
-            <input 
-                type="password" 
-                name="password" 
-                placeholder="Password"
-                className="text-lg p-2 border-2 border-black rounded-md mb-3"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-            />
-            <input 
-                type="password" 
-                name="cpassword" 
-                placeholder="Confirm Password"
-                className="text-lg p-2 border-2 border-black rounded-md mb-5"
-                value={cpassword}
-                onChange={e => setCPassword(e.target.value)}
-            />
-            <input 
-                type="submit" 
-                value="Sign Up"
-                className="bg-gray-950 text-white py-2 px-6 text-lg rounded-md cursor-pointer" 
-            />
-        </form>
-
-        <div className="text-center">
-            <p className="text-lg">Already have an account?</p>
-            <Link to="/sign-in" className="text-gray-950 text-lg italic underline">Sign In</Link>
-        </div>
-
+          Sign In
+        </Link>
+      </div>
     </section>
-    </section>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
